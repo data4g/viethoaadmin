245d5b1a14b5b57f57d605243e7e87cd04adebfc
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-27 21:41:28 +0700

    Update viethoadmin.sh

--- viethoadmin.sh
+++ viethoadmin.sh
@@ -1,5 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 cd /www/wwwroot/4gnguyennghi.xyz/public/assets/admin
 rm -rf umi.js
 curl -OL https://raw.githubusercontent.com/Nghi235/viethoaadmin/main/umi.js
 cd ../../../../../..
+clear
+echo -e ""
+echo -e ""
+echo -e "     Việt Hóa ADMIN Thành Công !"
